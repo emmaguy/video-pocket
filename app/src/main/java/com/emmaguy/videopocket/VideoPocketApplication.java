ea9fc6e34d91f8a59136ed505ad3330d98927916
Emma Guy <emmag87@gmail.com>
2016-03-25 16:08:22 +0000

    Try out android dev metrics

--- app/src/main/java/com/emmaguy/videopocket/VideoPocketApplication.java
+++ app/src/main/java/com/emmaguy/videopocket/VideoPocketApplication.java
@@ -1,6 +1,7 @@
 package com.emmaguy.videopocket;
 
 import com.emmaguy.videopocket.common.base.BaseApplication;
+import com.frogermcs.androiddevmetrics.AndroidDevMetrics;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
 import timber.log.Timber;
@@ -9,6 +10,10 @@ public final class VideoPocketApplication extends BaseApplication {
 
     @Override public void onCreate() {
         super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            AndroidDevMetrics.initWith(this);
+        }
 
         AndroidThreeTen.init(this);
 
